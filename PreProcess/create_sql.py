ceab10ca887573786cea48c409065a8441d7c6aa
babu406 <babuvgiridar@gmail.com>
2020-04-10 01:51:01 -0700

    Preprocess3
    finished preprocess code

--- PreProcess/create_sql.py
+++ PreProcess/create_sql.py
@@ -1,70 +1,88 @@
-import pymysql
-import psycopg2
 import csv
+import pymysql
 
-def creat_db():
+# defined init database
+def init():
     con = pymysql.connect(host='127.0.0.1', user='root',
-                          passwd='babu1', charset='utf8',use_unicode=True)
+                          passwd='babu1', charset='utf8')
     cur = con.cursor()
     cur.execute('DROP DATABASE IF EXISTS chart;')
     cur.execute('CREATE DATABASE IF NOT EXISTS chart;')
     con.commit()
     con.close()
-# def getOpenConnection(user='postgres', password='babu1', dbname='postgres1'):
-#     return psycopg2.connect("dbname='" + dbname + "' user='" + user + "' host='localhost' password='" + password + "'")
 
-def creat_tb(title, site, date, text):
-    con = pymysql.connect(host='127.0.0.1', user='root',
-                          passwd='babu1', db='chart', charset='utf8',use_unicode=True)
+
+# create new 'chart' table
+def insert_news(title, site, date, text):
+    con = pymysql.connect(host='127.0.0.1', user='root', 
+    passwd='babu1', db='chart', charset='utf8')
+    # , use_unicode=True
     cur = con.cursor()
-    try:
-        with con:
-            cur.execute("CREATE TABLE IF NOT EXISTS News(Id INT PRIMARY KEY AUTO_INCREMENT, Title varchar(1500), Site char("
-                        "255), Date char(40), Text varchar(5000))")
-            cur.execute(
-                'INSERT INTO News(Title, Site, Date, Text) VALUES(%s, %s, %s, %s)', (title, site, date, text))
-
-        con.commit()
-    except Exception as e:
-        con.rollback()
-        print(e)
+    
+    with con:
+        cur.execute('CREATE TABLE IF NOT EXISTS News(Id INT PRIMARY KEY AUTO_INCREMENT, Title varchar(150), Site char('
+                    '50), Date datetime, Text varchar(500))')
+        cur.execute('INSERT INTO News(Title, Site, Date, Text) VALUES(%s, %s, %s, %s)', (title, site, date, text))
 
     con.commit()
     con.close()
 
-def export_hourly_data(filepath):
+
+def insert_chart():
+    con = pymysql.connect(host='127.0.0.1', user='root',
+                          passwd='babu1', db='chart', charset='utf8')
+    cur = con.cursor()
+
+    with con:
+        cur.execute('CREATE TABLE IF NOT EXISTS APPL(DATE char(50), Time char(50), Open char(10), High char(10), '
+                    'Low char(10), Close char(10), fq char(15), Label char(5))')
+        # cur.execute("LOAD DATA INFILE '/Users/matthewbao/Desktop/study/CSE573/CHARTS/AMAZON60.csv' INTO TABLE "
+        #             "APPL fields terminated by ',' optionally enclosed by '"' escaped by '"' lines terminated by '\r\n'")
+    con.commit()
+    con.close()
+
+
+# insert news hourly
+def div_hourly_data(filepath, num):
     con = pymysql.connect(host='127.0.0.1', user='root',
                           passwd='babu1', db='chart', charset='utf8')
     cur = con.cursor()
     i = 0
-    #
-    while i < 10:
-        print(i)
-        try:
-            cur.execute("SET @dt = '2018-1-01 00:00:00'")
-            cur.execute("SET @dt1 = '%s'", i)
-            cur.execute("SET @dt2 = '%s'", i+1)
+    k = 0
+    res = open(filepath, 'w')
 
-            cur.execute("SELECT Title,Site, date_format(Date,'%Y-%m-%d %H:%i:%s') FROM News WHERE Date >= "
-                        "date_add(@dt, INTERVAL @dt1 HOUR) AND  DATE < date_add(@dt, INTERVAL @dt2 HOUR) ORDER BY "
+    while k < num-1:
+        try:
+            # set begin time and set the time interval (can change i+2 in two hours data)
+            cur.execute("SET @dt = '2017-12-8 00:00:00'")
+            cur.execute("SET @dt1 = '%s'", i)
+            cur.execute("SET @dt2 = '%s'", i + 1)
+            cur.execute("SELECT date_format(Date,'%Y.%m.%d %H:%i:%s'), Site, Text FROM News WHERE Date > "
+                        "date_add(@dt, INTERVAL @dt1 HOUR) AND  DATE <= date_add(@dt, INTERVAL @dt2 HOUR) ORDER BY "
                         "Date ASC")
 
             results = cur.fetchall()
-            export_data_toFile(results, filepath)
-            for r in results:
-                print(r)
-            
+            res = export_data_toFile(results, filepath)
+            # for r in results:
+            #     print(r)
+
             con.commit()
         except Exception as e:
             con.rollback()
-            print(e)
-        i = i+1
+            # print(e)
+        i = i + 1
+        k = k + len(results)
+        
+    res.close()
     con.close()
+
 
 # export data to csv file
 def export_data_toFile(results, filepath):
     with open(filepath, 'a+', newline='') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        for row in results:
-            csvwriter.writerow(row)
+        if len(results) != 0:
+            for row in results:
+                csvwriter.writerow(row)
+    return csvfile
 
--- PreProcess/open_file.py
+++ PreProcess/open_file.py
@@ -12,6 +12,12 @@ from nltk import word_tokenize, PorterStemmer
 from nltk.corpus import stopwords
 
 
+basepath = r'C:/Users/babuvgiridar/OneDrive - Arizona State University/Asu/Year/Master/Spring 2020/Cse 573/Project/Project_preprocess'
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+# print(ROOT_DIR)
+
+total_Column_step = 0
+
 def preprocess_data(text):
     # remove space
     sentence = text.strip()
@@ -31,8 +37,19 @@ def preprocess_data(text):
     # stem word
     porter = PorterStemmer()
     sentence = [porter.stem(word) for word in sentence]
-    result = ','.join(sentence)
-    return result
+    # result = ','.join(sentence)
+    result1 = [x.replace("",'') for x in sentence]
+    while('' in result1 ):
+        result1.remove('')
+
+    # print(result1)
+    
+    #result = ','.join(sentence)
+    
+    result1 = ','.join(result1)
+    # print(result1)
+    return result1
+    # return result
 
 
 def time_fix(dt):
@@ -51,8 +68,9 @@ def write_file(filepath):
     my_files = []
     my_files2 = []
     counter_files = 0
-    # rootdir = r'C:\Users\babuvgiridar\OneDrive - Arizona State University\Asu\Year\Master\Spring 2020\Cse 573\Project\Project_preprocess\Data\News'
-    rootdir = r'C:\Users\babuvgiridar\OneDrive - Arizona State University\Asu\Year\Master\Spring 2020\Cse 573\Project\Project_preprocess\Data\News\2018_01_d157b48c57be246ec7dd80e7af4388a2'
+    # rootdir = basepath + '/Data/News/2018_01_d157b48c57be246ec7dd80e7af4388a2'
+    # rootdir = basepath + '/Data/Sample_test'
+    rootdir = basepath + '/Data/News/'
 
     for subdirectory, dirs, files in os.walk(rootdir):
         for file in files:
@@ -63,8 +81,10 @@ def write_file(filepath):
                 my_files2.append(str(filepath))
                 counter_files += 1
 
+    global total_Column_step
+    total_Column_step = counter_files
     print("File #:", counter_files)
-    create_sql.creat_db()
+    create_sql.init()
     for i in my_files2:
         with open(i, 'r', errors='ignore') as f:
             # print(i)
@@ -74,17 +94,14 @@ def write_file(filepath):
             site = data['thread']['site']
             date1 = time_fix(data['thread']['published'])
             date = time_fix(data['thread']['published']).strftime("%Y,%m,%d,%H,%M,%S")
-            print(date)
-            # print(title)
-            inset_db(title, site, date1, text)
+            # print(date)
+            # print(text)
+            create_sql.insert_news(title, site, date, text)
             lists = '[' + title + ']' + ', [' + site + '], [' + date + '], [' + text + '] \n'
         test.write(lists + '\n')
         # print(lists)
     test.close()
 
-
-def inset_db(title, site, date, text):
-    create_sql.creat_tb(title, site, date, text)
 
 def openClose(chart_filename):
     chartData = open(chart_filename, 'a+')
@@ -93,30 +110,19 @@ def openClose(chart_filename):
         print(row)
     chartData.close()
 
-def timeNewsCorr(newsData, stockDatApple, stockDatAmazon):
 
-    newsData1 = open(newsData, 'r', errors='ignore')
-    appleData = open(stockDatApple, 'r', errors='ignore')
+def timeNewsCorr(newsData, stockDatAmazon):
+    newsData_read = open(newsData, 'r', errors='ignore')
     amazonData = open(stockDatAmazon, 'r', errors='ignore')
 
     news_totalData = []
-    news_reader = csv.reader(newsData1)
+    label_amazon = []
+    news_reader = csv.reader(newsData_read)
+
     for line in news_reader:
         news_totalData.append(line[0])
-    # print(news_totalData[0], "\n")
+    newsData_read.close()
 
-    apple_date = []
-    apple_totalDataStart = []
-    apple_totalDataEnd = []
-    apple_delta = []
-    appleReader = csv.reader(appleData)
-    for line in appleReader:
-        apple_date.append(line[0])
-        apple_totalDataStart.append(line[2])
-        apple_totalDataEnd.append(line[5])
-        apple_delta.append(line[7])
-    # print("Apple: " ,apple_totalDataStart[0] ,"\n", apple_totalDataStart[0], "\n", apple_totalDataEnd[0],"\n" , apple_date[0])
-    
     amazon_date = []
     amazon_time = []
     amazon_totalDataStart = []
@@ -133,67 +139,93 @@ def timeNewsCorr(newsData, stockDatApple, stockDatAmazon):
 
     start_Time = datetime.strptime("12:30", "%H:%M")
     end_Time = datetime.strptime("21:30", "%H:%M")
-    # print(start_Time, " ", end_Time)
+
     amazon_delta_complete = []
-    apple_delta_complete = []
-    flag = 0
+
     for counter, row in enumerate(news_totalData):
         flag = 0
-        news_date_var = datetime.strptime(row, "%Y-%m-%d %H:%M:%S")
+        news_date_var = datetime.strptime(row, "%Y.%m.%d %H:%M:%S")
         # print("news date:",news_date_var)
         for counter2, row2 in enumerate(amazon_date):
-            first_amazon = datetime.strptime(row2+ "," + amazon_time[counter2], "%Y.%m.%d,%H:%M")
+            first_amazon = datetime.strptime(row2 + "," + amazon_time[counter2], "%Y.%m.%d,%H:%M")
             # print(news_date_var , " ", first.date())
-            if (news_date_var.date() == first_amazon.date()): 
+            if news_date_var.date() == first_amazon.date():
                 # if it is in 13:30-21:30
-                if(news_date_var.time() >= start_Time.time() and news_date_var.time() < end_Time.time()):
+                if start_Time.time() <= news_date_var.time() < end_Time.time():
                     news_date_var2 = news_date_var + timedelta(hours=1)
-                    if(news_date_var.time() <= first_amazon.time() and news_date_var2.time() > first_amazon.time()):
+                    if news_date_var.time() <= first_amazon.time() < news_date_var2.time():
                         amazon_delta_complete.append(amazon_delta[counter2])
-                        # print("found exact hr:" , first_amazon.time() , "||" ,news_date_var )
-                        # print(amazon_delta[counter2], "counter:" , counter, "||", counter2)
+                        print("found exact hr:", first_amazon.time(), "||", news_date_var,
+                              "label:", amazon_delta[counter2], "counter:", counter, "||", counter2)
+                        label_amazon.append(amazon_delta[counter2])
                         flag = 1
                 # this accounts for not in 13:30-21:30 so +1 day 1st hr
                 else:
                     temp = news_date_var + timedelta(days=1)
-                    # temp2 = temp.strptime(temp+"12:30" , "%Y:%M:%d %H:%M")
                     temp2 = temp.replace(hour=13, minute=30)
-                    temp3 = temp2.strptime(row, "%Y-%m-%d %H:%M:%S")
+                    temp3 = temp2.strptime(row, "%Y.%m.%d %H:%M:%S")
                     for counter3, row3 in enumerate(amazon_date):
-                        first_amazon2 = datetime.strptime(row3+ "," + amazon_time[counter3], "%Y.%m.%d,%H:%M")
+                        first_amazon2 = datetime.strptime(row3 + "," + amazon_time[counter3], "%Y.%m.%d,%H:%M")
                         # print("hey:" , first_amazon2 , "||", temp2)
                         # if(temp2.date() == first_amazon2.date() and temp2.time() == first_amazon2.time()):
-                        if(temp2 == first_amazon2 and flag != 1):
+                        if temp2 == first_amazon2 and flag != 1:
                             amazon_delta_complete.append(amazon_delta[counter3])
-                            print("Found 2 ", news_date_var, "||+1 day", temp2, "||amazon: " , first_amazon2)
-                            print(amazon_delta[counter3], "counter", counter , "||", counter3)
+                            print("Found 2 ", news_date_var, "||+1 day", temp2, "||amazon: ", first_amazon2,
+                                  "label:", amazon_delta[counter3], "counter", counter, "||", counter3)
+                            label_amazon.append(amazon_delta[counter3])
                             flag = 1
-                            
-        # this accounts for holidays/if not in amazon do +1 day and 13:30 and give that 
-        if (flag == 0):
+
+        # this accounts for holidays/if not in amazon do +1 day and 13:30 and give that
+        if flag == 0:
             temp3 = news_date_var + timedelta(days=1)
             temp4 = temp3.replace(hour=13, minute=30)
+
+            temp5 = news_date_var + timedelta(days=8)
+            temp6 = temp5.replace(hour=13, minute=30)
             # print("holiday:", news_date_var, "||", temp4)
             for counter4, row4 in enumerate(amazon_date):
-                    first_amazon3 = datetime.strptime(row4 + "," + amazon_time[counter4], "%Y.%m.%d,%H:%M")
-                    # print("hey:" , first_amazon3 , "||", temp4)
-                    if(temp4.date() == first_amazon3.date()):
-                        # print("time", temp2.time(), first_amazon2.time())
-                        if(temp4.time() == first_amazon3.time()):
-                            amazon_delta_complete.append(amazon_delta[counter4])
-                            # print("Found 3 ", news_date_var, "||+1 day", temp4, "||amazon: " , first_amazon3)
-                            # print(amazon_delta[counter4], "counter", counter , "||", counter4)            
-
-    # print(amazon_delta_complete[67])
-     
-
-# folder_path = '/Users/matthewbao/Desktop/study/CSE573/News/2018_01_d157b48c57be246ec7dd80e7af4388a2'
-txt_filename = r'C:\Users\babuvgiridar\OneDrive - Arizona State University\Asu\Year\Master\Spring 2020\Cse 573\Project\Project_preprocess\test.txt'
+                first_amazon3 = datetime.strptime(row4 + "," + amazon_time[counter4], "%Y.%m.%d,%H:%M")
+                # print("hey:" , first_amazon3 , "||", temp4)
+                if temp4.date() <= first_amazon3.date() <= temp6.date():
+                    # print("time", temp2.time(), first_amazon2.time())
+                    if temp4.time() <= first_amazon3.time() <= temp6.time():
+                        amazon_delta_complete.append(amazon_delta[counter4])
+                        print("Found 3 ", news_date_var, "||+1 day", temp4, "||amazon: ", first_amazon3,
+                              "label:", amazon_delta[counter4], "counter", counter, "||", counter4)
+                        label_amazon.append(amazon_delta[counter4])
+    return label_amazon
+print("part1: preprocess: write to txt ")
+# step 1
+# txt_filename =  r'C:\Users\babuvgiridar\OneDrive - Arizona State University\Asu\Year\Master\Spring 2020\Cse 573\Project\Project_preprocess\test.txt'
+txt_filename = basepath + "/test.txt"
 # write_file(txt_filename)
-chart_filename = r'C:\Users\babuvgiridar\OneDrive - Arizona State University\Asu\Year\Master\Spring 2020\Cse 573\Project\Data\CHARTS\AMAZON60.csv'
-# openClose(chart_filename)
-newsData1 = r'C:\Users\babuvgiridar\OneDrive - Arizona State University\Asu\Year\Master\Spring 2020\Cse 573\Project\Project_preprocess\test1.csv'
-stockDataApple1 = r'C:\Users\babuvgiridar\OneDrive - Arizona State University\Asu\Year\Master\Spring 2020\Cse 573\Project\Project_preprocess\Data\CHARTS\APPLE60.csv'
-stockDataAmazon1 = r'C:\Users\babuvgiridar\OneDrive - Arizona State University\Asu\Year\Master\Spring 2020\Cse 573\Project\Project_preprocess\Data\CHARTS\AMAZON60.csv'
-timeNewsCorr(newsData1, stockDataApple1, stockDataAmazon1)
-    
+
+# step 2
+# chart_filename = basepath + '/Data/CHARTS/AMAZON60.csv'
+create_file_csv = basepath + '/test1.csv'
+print("total1:", total_Column_step)
+# create_sql.div_hourly_data(create_file_csv, total_Column_step)
+print("part2 done")
+
+print("part3: calculate delta of hour")
+# Step 3
+newsData1 = basepath + '/test1.csv'
+stockDataApple1 = basepath + '/Data/CHARTS/2APPLE60.csv'
+stockDataAmazon1 = basepath + '/Data/CHARTS/2AMAZON60.csv'
+label_file1 = basepath+'/test2.csv'
+
+label_amazon = timeNewsCorr(newsData1, stockDataApple1)
+print("Apple\n")
+label_apple = timeNewsCorr(newsData1, stockDataApple1)
+
+with open(newsData1) as csvfile:
+    rows = csv.reader(csvfile)
+    with open(label_file1, 'w', newline='') as f:
+        writer = csv.writer(f)
+        i = 0
+        for row in rows:
+            row.append(label_amazon[i])
+            row.append(label_apple[i])
+            i = i + 1
+            print(row)
+            writer.writerow(row)
