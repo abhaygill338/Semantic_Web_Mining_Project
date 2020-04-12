ab88e9c9afec5b2f7e32cf3258fd691ec0acc9a2
abhaygill338 <abhaygill206@gmail.com>
2020-04-11 19:02:15 -0700

    New Training Model Non-linear SVM is added.

---
+++ CODE/TrainingModels/SVM_Amazon.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+def preprocess():
+	df = pd.read_csv('featureMatrix(amazon and apple).csv')
+	df1 = df[df['Company Stocks'] == 1]
+	X = df1[['Semantic Analysis']]
+	y = df1[['Amazon']]
+
+	return X,y
+
+def ImplementSVM(X,y):
+	clf = svm.NuSVC(gamma='auto')
+	clf.fit(X, y)
+	predicted_classes = clf.predict(X)
+	accuracy = accuracy_score(y,predicted_classes)
+	print("Accuracy: ",str(accuracy*100)+"%")
+
+
+
+if __name__ == '__main__':
+	X,y = preprocess()
+	ImplementSVM(X,y)
