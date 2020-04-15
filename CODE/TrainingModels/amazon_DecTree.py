61d420d0347fde3e5abc36ce9acdb73949bdbde4
Palash-Mehta <palashmehta1397@gmail.com>
2020-04-15 01:23:14 -0700

    Code run on new data and genrate new plots

--- CODE/TrainingModels/amazon_DecTree.py
+++ CODE/TrainingModels/amazon_DecTree.py
@@ -1,52 +1,55 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Apr 11 12:16:33 2020
+Created on Tue Apr 14 23:41:43 2020
 
-@author: palash
+@author: palas
 """
 
 #IMPORTING REQUIRED LIBRARIES
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
+from sklearn import preprocessing,svm
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score,f1_score,confusion_matrix,classification_report
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score,f1_score,precision_recall_curve,classification_report
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import precision_recall_curve
+
 
 # READING DATA AND EXTRACTING RELEVANT COLUMNS
-df = pd.read_csv('Updated_2_featureMatrix(amazon and apple).csv')
-df1 = df.loc[(df['Company Stocks'] == 2) | (df['Company Stocks'] == 3)]
+df = pd.read_csv('featurematrix.csv')
+df1 = df.loc[(df['Company Stocks'] == 1) | (df['Company Stocks'] == 3)]
 X = df1[['Semantic Analysis','AccuracySites']].values
-y = df1[['Apple']].values
-
+y = df1[['Amazon']].values
 
 #PLOTTING THE TARGET TO SEE THE RATIO OF POSTIVES AND NEGATIVES
-df1['Apple'].value_counts()
-sns.countplot(x='Apple',data = df1, palette = 'hls')
-plt.show()
+df1['Amazon'].value_counts()
+sns.countplot(x='Amazon',data = df1, palette = 'hls')
+
+
+#PLOTTING FEATURES AGAINST THE LABELS
+sns.set_style('whitegrid')
+sns.countplot(x='Amazon', hue='Semantic', data=df1)
 
 
 # PRINT THE PERCENTAGE OF NEGATIVES AND POSITIVES
-stock_dec = len(df1[df1['Apple']== -1])
-stock_inc = len(df1[df1['Apple']== 1])
+stock_dec = len(df1[df1['Amazon']== -1])
+stock_inc = len(df1[df1['Amazon']== 1])
 pct_stock_dec = stock_dec/(stock_dec+stock_inc)
 print("Percentage of stock decrease is", pct_stock_dec*100)
 pct_stock_inc = stock_inc/(stock_inc+stock_dec)
 print("percentage of stock increase is", pct_stock_inc*100)
 
 
-# lOGISTIC REGRESSION
-
+#DECISION TREE
 X_train,X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=0)
 min_max_scaler = preprocessing.MinMaxScaler()
 x_train_scaled = min_max_scaler.fit_transform(X_train)
 x_test_scaled = min_max_scaler.fit_transform(X_test)
-logreg = LogisticRegression()
-logreg.fit(X_train,y_train)
-y_pred = logreg.predict(X_test)
-y_train_pred = logreg.predict(X_train)
+tree = DecisionTreeClassifier()
+tree.fit(X,y)
+y_pred = tree.predict(X_test)
+y_train_pred = tree.predict(X_train)
 print('Training accuracy %s' % accuracy_score(y_train, y_train_pred))
 print('Testing F1 score: {}'.format(f1_score(y_train, y_train_pred, average='weighted')))
 print('Testing accuracy %s' % accuracy_score(y_test, y_pred))
@@ -60,13 +63,12 @@ def plot_prec_recall_vs_tresh(precisions, recalls, thresholds):
     plt.xlabel('Threshold')
     plt.legend(loc='upper left')
     plt.ylim([0,1])
-    plt.show()
 
-pre, rec, thresholds = precision_recall_curve(y_test, logreg.predict_proba(X_test)[:,1])
+pre, rec, thresholds = precision_recall_curve(y_test, tree.predict_proba(X_test)[:,1])
 plot_prec_recall_vs_tresh(pre, rec, thresholds)
 plt.figure()
 
-#LOGISTIC REGRESSION REPORT
+
+#DECSION TREE REPORT
 report = classification_report(y_test,y_pred,output_dict=True)
 report_df = pd.DataFrame(report).transpose()
-print(report_df)
--- CODE/TrainingModels/amazon_LR.py
+++ CODE/TrainingModels/amazon_LR.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Apr 11 12:16:33 2020
+Created on Thu Apr  9 13:40:33 2020
 
-@author: abhay
+@author: palash
 """
 
 #IMPORTING REQUIRED LIBRARIES
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn import svm
-from sklearn import preprocessing
-from sklearn.metrics import accuracy_score,f1_score,confusion_matrix,classification_report
+from sklearn import preprocessing,svm
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score,f1_score,precision_recall_curve,classification_report
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import precision_recall_curve
+
 
 # READING DATA AND EXTRACTING RELEVANT COLUMNS
-df = pd.read_csv('Updated_2_featureMatrix(amazon and apple).csv')
-df1 = df.loc[(df['Company Stocks'] == 2) | (df['Company Stocks'] == 3)]
+df = pd.read_csv('featurematrix.csv')
+df1 = df.loc[(df['Company Stocks'] == 1) | (df['Company Stocks'] == 3)]
 X = df1[['Semantic Analysis','AccuracySites']].values
 y = df1[['Amazon']].values
 
@@ -25,7 +26,12 @@ y = df1[['Amazon']].values
 #PLOTTING THE TARGET TO SEE THE RATIO OF POSTIVES AND NEGATIVES
 df1['Amazon'].value_counts()
 sns.countplot(x='Amazon',data = df1, palette = 'hls')
-plt.show()
+
+
+#PLOTTING THE FEATURES AGAINST THE LABELS
+sns.set_style('whitegrid')
+sns.countplot(x='Amazon', hue='Semantic', data=df1)
+sns.countplot(x='Amazon',hue='Accuracy',data=df1)
 
 
 # PRINT THE PERCENTAGE OF NEGATIVES AND POSITIVES
@@ -37,17 +43,15 @@ pct_stock_inc = stock_inc/(stock_inc+stock_dec)
 print("percentage of stock increase is", pct_stock_inc*100)
 
 
-
-
-#DECISION TREE
+# lOGISTIC REGRESSION
 X_train,X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=0)
 min_max_scaler = preprocessing.MinMaxScaler()
 x_train_scaled = min_max_scaler.fit_transform(X_train)
 x_test_scaled = min_max_scaler.fit_transform(X_test)
-clf = svm.NuSVC(gamma='auto',probability = True)
-clf.fit(X, y)
-y_pred = clf.predict(X_test)
-y_train_pred = clf.predict(X_train)
+logreg = LogisticRegression()
+logreg.fit(X_train,y_train)
+y_pred = logreg.predict(X_test)
+y_train_pred = logreg.predict(X_train)
 print('Training accuracy %s' % accuracy_score(y_train, y_train_pred))
 print('Testing F1 score: {}'.format(f1_score(y_train, y_train_pred, average='weighted')))
 print('Testing accuracy %s' % accuracy_score(y_test, y_pred))
@@ -61,14 +65,19 @@ def plot_prec_recall_vs_tresh(precisions, recalls, thresholds):
     plt.xlabel('Threshold')
     plt.legend(loc='upper left')
     plt.ylim([0,1])
-    plt.show()
 
-pre, rec, thresholds = precision_recall_curve(y_test, clf.predict_proba(X_test)[:,1])
+pre, rec, thresholds = precision_recall_curve(y_test, logreg.predict_proba(X_test)[:,1])
 plot_prec_recall_vs_tresh(pre, rec, thresholds)
 plt.figure()
 
 
-#DECISION TREE REPORT
+#LOGISTIC REGRESSION REPORT
 report = classification_report(y_test,y_pred,output_dict=True)
 report_df = pd.DataFrame(report).transpose()
-print(report_df)
+
+
+
+
+
+
+
