f0c436d56382484974ef5250cade2e844ed8e901
Zhenyu Bao <31489480+Yorkbaobao@users.noreply.github.com>
2020-04-16 15:21:43 -0700

    f1 added

--- CODE/TrainingModels/RNN_Amazon.py
+++ CODE/TrainingModels/RNN_Amazon.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Embedding, LSTM, Dense, Dropout
@@ -7,27 +8,26 @@ from keras.preprocessing.sequence import pad_sequences
 import tensorflow as tf
 
 
-# 2 apple, 1 amazon
+# 1 amazon, 2 apple
+# 1 hour --> ac = 87.51%, f1 = 81%
+# 4 hours --> ac = 85.49%, f1 = 80.95%
 def get_value():
-	df = pd.read_csv('featureMatrix(amazon and apple).csv')
-	df1 =df.loc[(df['Company Stocks'] == 1) | (df['Company Stocks'] == 3)]
-	y = df1[['Positive Analysis']]
-	X = df1['Text']
-	# print(X.str.len())
-	print(X)
+	df = pd.read_csv('Updated_2_featureMatrix(amazon and apple).csv', encoding='latin-1')
+	# df = pd.read_csv('featureMatrix(amazon and apple).csv', encoding='latin-1')
+	df1 = df.loc[(df['Company Stocks'] == 1) | (df['Company Stocks'] == 3)]
+	y = df1[['Positive semantic']].values
+	X = df1['Text'].values
 	return X, y
 
 
 def rnn(X, y):
+	# preprocessing data
 	tk = Tokenizer(lower=True)
 	tk.fit_on_texts(X)
 	X_seq = tk.texts_to_sequences(X)
 	X_pad = pad_sequences(X_seq, maxlen=100, padding='post')
 
-	print(len(X_pad))
-	# print(y)  32912
-
-	X_train, X_test, y_train, y_test = train_test_split(X_pad, y, test_size=0.25, random_state=1)
+	X_train, X_test, y_train, y_test = train_test_split(X_pad, y, test_size=0.25, random_state=0)
 	batch_size = 64
 	X_train1 = X_train[batch_size:]
 	y_train1 = y_train[batch_size:]
@@ -35,7 +35,6 @@ def rnn(X, y):
 	y_valid = y_train[:batch_size]
 
 	vocabulary_size = len(tk.word_counts.keys())+1
-	# print(vocabulary_size) 85024
 
 	# create the model
 	max_words = 100
@@ -46,14 +45,20 @@ def rnn(X, y):
 	model.add(LSTM(200))
 	model.add(Dense(1, activation='sigmoid'))
 	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	model.fit(X_train1, y_train1, validation_data=(X_valid, y_valid), batch_size=batch_size, epochs=5)
+	# model.save('stock_apple.h5')
+	# model = load_model('stock_apple.h5')
 
 	# evaluate the training model
-	model.fit(X_train1, y_train1, validation_data=(X_valid, y_valid), batch_size=batch_size, epochs=10)
 	accuracy = model.evaluate(X_test, y_test)
 	print(accuracy)
+
+	# calculate F1 score
+	yhat_classes = model.predict_classes(X_valid, verbose=0)
+	f1 = f1_score(y_valid, yhat_classes)
+	print('F1 score: %f' % f1)
 
 
 if __name__ == '__main__':
 	X, y = get_value()
 	rnn(X, y)
-
