4347afe72fc52c8f6dbc5229f0e8c7c7785cd4e8
YorkBao <yorkbaozy@gmail.com>
2020-04-16 13:40:19 -0700

    rnn
    add input and dataset about positive semantic

--- CODE/TrainingModels/RNN_Apple.py
+++ CODE/TrainingModels/RNN_Apple.py
@@ -1,42 +1,48 @@
 import pandas as pd
+import numpy as np
+from keras.engine.saving import load_model
+from keras.losses import mean_squared_error
+from matplotlib import pyplot
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Embedding, LSTM, Dense, Dropout
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
+
+scaler = MinMaxScaler(feature_range=(0, 1))
 
 
 # 2 apple, 1 amazon
-def preprocess():
-    df = pd.read_csv('featureMatrix(amazon and apple).csv')
-    df1 = df[df['Company Stocks'] == 2]
-    y = df1[['Semantic Analysis']]
-    X = df1['Text']
-    # print(X.str.len())
-    # print(X)
+# 'featureMatrix.csv to four hours -->
+# 'Updated_2_featureMatrix(amazon and apple).csv' to one hour --> 88.13%
+def get_value():
+    df = pd.read_csv('Updated_2_featureMatrix(amazon and apple).csv', encoding='latin-1')
+    # df = pd.read_csv('featureMatrix(amazon and apple).csv', encoding='latin-1')
+    df1 = df.loc[(df['Company Stocks'] == 2) | (df['Company Stocks'] == 3)]
+    y = df1[['Positive semantic']].values
+    X = df1['Text'].values
     return X, y
 
 
 def rnn(X, y):
+    # preprocessing data
     tk = Tokenizer(lower=True)
     tk.fit_on_texts(X)
     X_seq = tk.texts_to_sequences(X)
     X_pad = pad_sequences(X_seq, maxlen=100, padding='post')
-
-    print(len(X_pad))
+    # to normalize the dataset
+    # X_nor = scaler.fit_transform(X_pad)
+    # print(len(X_pad))
     # print(y)  32912
 
-    X_train, X_test, y_train, y_test = train_test_split(X_pad, y, test_size=0.25, random_state=1)
+    X_train, X_test, y_train, y_test = train_test_split(X_pad, y, test_size=0.25, random_state=0)
     batch_size = 64
     X_train1 = X_train[batch_size:]
     y_train1 = y_train[batch_size:]
     X_valid = X_train[:batch_size]
     y_valid = y_train[:batch_size]
-    # print(X_train1)
-    # print(y_train1)
-    # print(X_valid)
-    # print(y_valid)
 
     vocabulary_size = len(tk.word_counts.keys()) + 1
     # print(vocabulary_size) 85024
@@ -46,16 +52,24 @@ def rnn(X, y):
     embedding_size = 32
     model = Sequential()
     model.add(Embedding(vocabulary_size, embedding_size, input_length=max_words))
+    model.add(Dropout(0.5))
     model.add(LSTM(200))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(X_train1, y_train1, validation_data=(X_valid, y_valid), batch_size=batch_size, epochs=5)
+    # model.save('stock_apple.h5')
+    # model = load_model('stock_apple.h5')
+
+    # model predict
+    y_predict = model.predict(X_test)
+    print(y_predict)
 
     # evaluate the training model
-    model.fit(X_train1, y_train1, validation_data=(X_valid, y_valid), batch_size=batch_size, epochs=10)
     accuracy = model.evaluate(X_test, y_test)
     print(accuracy)
+    # print("model mean squared error is " + str(mean_squared_error(y_test, y_predict)))
 
 
 if __name__ == '__main__':
-    X, y = preprocess()
+    X, y = get_value()
     rnn(X, y)
